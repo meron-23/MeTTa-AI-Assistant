6bf9e117780c2dffd630621dd90eb5add8dbe8bb
abnsol <abenisolo51196@gmail.com>
2025-10-01 03:53:24 +0300

    Change postgresdb to mongodb

--- Experiment/AST-Based-Recursive-Chunker/chunker.py
+++ Experiment/AST-Based-Recursive-Chunker/chunker.py
@@ -1,6 +1,6 @@
+import asyncio
 import os
 import argparse
-import psycopg
 import metta_ast_parser 
 from typing import List
 from db import Database as DB
@@ -14,7 +14,7 @@ def getSize(node: metta_ast_parser.SyntaxNode) -> int:
     start, end = node.src_range
     return end - start
 
-def ChunkPreprocessedCode(potential_chunks, max_size: int, db: DB, source_code: str) -> List[str]:
+async def ChunkPreprocessedCode(potential_chunks, max_size: int, db: DB, source_code: str) -> List[str]:
     """Chunks a list of potential chunks based on max_size.
     Each potential chunk is a list of text_node table IDs from our db.
     Returns a list of chunked code strings.
@@ -24,7 +24,8 @@ def ChunkPreprocessedCode(potential_chunks, max_size: int, db: DB, source_code: 
     for chunk_ids in potential_chunks:
         chunk, chunk_size = [], 0
         for chunk_id in chunk_ids:
-            text_range = db.get_text_node(chunk_id)[1]
+            text_range = await db.get_text_node(chunk_id)
+            text_range = text_range["text_range"]
             text_node = source_code[text_range[0]:text_range[1]]
             text_size = len(text_node)
             
@@ -52,25 +53,24 @@ def ChunkPreprocessedCode(potential_chunks, max_size: int, db: DB, source_code: 
 
     return chunks
 
-def ChunkCode(code: str, max_size: int) -> List[str]:
+async def ChunkCode(code: str, max_size: int) -> List[str]:
     """
     Chunks the code into smaller pieces based on the max_size.
     Stores the chunks in the database.
     """
 
     load_dotenv() 
-    with psycopg.connect(os.getenv("POSTGRES_URL")) as conn:    
-        db = DB(conn)
-        db.recreate_schema()
-        db.clear_text_nodes()
-        db.clear_symbols()
-        
-        # for now lets pass the code to the chunker
-        # later we can add a feature to fetch the source code from the db using the file_path
-        potential_chunks = preprocess_code(code, "temp_path", db)
-        chunks = ChunkPreprocessedCode(potential_chunks, max_size, db, code)
-        ids = db.insert_chunks(chunks)
-        return chunks
+
+    db = DB(os.getenv("mongo_uri"), "MeTTa_Chunker")
+    "JUST FOR DEV"
+    await db.clear_all_collections()
+    
+    # for now lets pass the code to the chunker
+    # later we can add a feature to fetch the source code from the db using the file_path
+    potential_chunks = await preprocess_code(code, "temp_path", db)
+    chunks = await ChunkPreprocessedCode(potential_chunks, max_size, db, code)
+    ids = await db.insert_chunks(chunks)
+    return chunks
 
 def ChunkCodeRecursively(node: metta_ast_parser.SyntaxNode ,text: str, max_size: int) -> list[str]:
     """Recursively chunks a potential chunk (syntax node) when it exceeds max_size."""
@@ -94,7 +94,7 @@ def ChunkCodeRecursively(node: metta_ast_parser.SyntaxNode ,text: str, max_size:
     return chunks
 
 
-def main():
+async def main():
     parser = argparse.ArgumentParser(description="Chunk a MeTTa source file using an AST-based algorithm.")
     parser.add_argument("input_file", help="Path to the input .metta file to be chunked.")
     parser.add_argument("output_file", help="Path to the output file where chunks will be written.")
@@ -111,10 +111,10 @@ def main():
         return
 
     print(f"Chunking code with MAX_SIZE = {args.max_size}...")
-    final_chunks = ChunkCode(source_code, args.max_size)
-    print(final_chunks)
+    final_chunks = await ChunkCode(source_code, args.max_size)
+    # print(final_chunks)
     print("Chunks Stored in database")
     print("Chunking complete!")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
