0b899cd9e0aa41dbdb82e569725bc4316f60b48e
abnsol <abenisolo51196@gmail.com>
2025-10-04 16:41:53 +0300

    fix(ingest): import structure

--- Backend/app/core/repo_ingestion/ingest.py
+++ Backend/app/core/repo_ingestion/ingest.py
@@ -1,13 +1,10 @@
-import os,sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))  
-
-import shutil
-from clone import clone_repo, get_all_files
-from filters import process_metta_files
-from config import TEMP_DIR, DATA_DIR
-from loguru import logger
-from chunker.chunker import ast_based_chunker
 import asyncio
+from loguru import logger
+import shutil
+from .clone import clone_repo, get_all_files
+from .filters import process_metta_files
+from .config import TEMP_DIR, DATA_DIR
+from ..chunker import chunker
 
 def ingest_pipeline(repo_url: str) -> None:
     repo_path: str = clone_repo(repo_url, TEMP_DIR)
@@ -15,7 +12,7 @@ def ingest_pipeline(repo_url: str) -> None:
     try:
         files: list[str] = get_all_files(repo_path)
         indexes = process_metta_files(files, DATA_DIR, repo_root=repo_path)
-        asyncio.run(ast_based_chunker(indexes))
+        asyncio.run(chunker.ast_based_chunker(indexes))
     finally:
         logger.info(f"Cleaning up {repo_path}")
         shutil.rmtree(repo_path, ignore_errors=True)
@@ -23,6 +20,7 @@ def ingest_pipeline(repo_url: str) -> None:
 # TEST
 if __name__ == "__main__":
     # test_repo: str = "https://github.com/iCog-Labs-Dev/metta-moses.git"
-    test_repo: str = "https://github.com/iCog-Labs-Dev/hyperon-openpsi.git"
+    # test_repo: str = "https://github.com/iCog-Labs-Dev/hyperon-openpsi.git"
+    test_repo : str = "https://github.com/trueagi-io/hyperon-experimental"
 
     ingest_pipeline(test_repo)
