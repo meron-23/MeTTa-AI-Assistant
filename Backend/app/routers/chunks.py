771527dd5b47d27e967c17a8ed7595c69fb86b39
abel ashenafi <ashenafiabel66@gmail.com>
2025-10-11 15:47:12 +0300

    Resolve merge conflicts with upstream main

--- Backend/app/routers/chunks.py
+++ Backend/app/routers/chunks.py
@@ -1,9 +1,12 @@
 import os
-from fastapi import APIRouter, HTTPException, status, Depends, Query
+
 from typing import Optional, Dict, Any, List
 from pydantic import BaseModel
-from app.db.db import update_chunk, delete_chunk, get_chunk_by_id, get_chunks, ChunkSchema
+
 from pymongo.database import Database
+from fastapi import APIRouter, HTTPException, status, Depends, Query
+from ..core.repo_ingestion.ingest import ingest_pipeline
+from app.db.db import update_chunk, delete_chunk, get_chunk_by_id, get_chunks
 from app.dependencies import get_mongo_db, get_embedding_model_dep, get_qdrant_client_dep
 from app.embedding.pipeline import embedding_pipeline
 
